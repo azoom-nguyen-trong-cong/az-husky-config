9b858ed9b5ab6cc4c8032833eb79739f12e91cef
Nguyen Trong Cong (Kalenz) <124549628+azoom-nguyen-trong-cong@users.noreply.github.com>
2024-08-30 18:26:25 +0700

    Develop (#3)

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the Vuetify + Nuxt.js template hello
         </v-card-title>
         <v-card-text>
           <p>
